c01b98e03692427a6616f8e3d0cc91a3df6728c9
esza <joelespejel@gmail.com>
2022-03-03 23:49:56 -0600

    Se agrega la barra de navehacion a todas las vistas

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,70 @@
 
 
 const PI = 3.14159265;
+
+let barra = [
+    {
+        products: "../../list_of_products.html",
+        form: "../../form.html",
+        logo: "../../index.html",
+        us: "../../aboutUs.html",
+        contact: "../../contact_us.html"
+    }
+
+];
+
+
+
+    const contenidoBarra =`
+    <div class="container">
+      
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+    <!--<a class="navbar-brand" href="./index.html">Navbar</a>-->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="${barra.products}" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                    Productos
+                </a>                    
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Sombreros</a>
+                    <a class="dropdown-item" href="#">Gorras</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="${barra.form}">Formulario<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">                    
+                <a class="navbar-brand" href="${barra.logo}">
+                    <img src="./src/img/tutuLogo.png" width="80" height="32" class="d-inline-block align-top" alt="Logo tutu_wix">
+                </a>
+            </li>        
+            <li class="nav-item">
+                <a class="nav-link" href="${barra.us}">Nosotros</a>
+            </li>            
+            <li class="nav-item">
+                <a class="nav-link" href="${barra.contact}l">Contactanos</a>
+            </li>                      
+        </ul>
+    <!--<form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>-->
+    </div>
+    <div class="d-flex">
+      <a href="./login.html">
+        <img src="./src/img/usuario.png" class="mr-2" width="30" height="30" src="./usuario.png" alt="Logo">
+      </a>                
+    </div>
+</nav>
+
+</div>
+    `;
+     
+
+
+let anclaBarra = document.getElementById("anclaBarra");
+anclaBarra.innerHTML = contenidoBarra;
