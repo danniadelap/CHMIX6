9e1c7670af16c962db407bc4830202cd2d1f7127
JuanJo <jjms2508@gmail.com>
2022-03-29 01:26:09 -0600

    Se agrega validacion de contraseña y ayuda visual.

--- src/js/form_admin.js
+++ src/js/form_admin.js
@@ -84,5 +84,79 @@ form.addEventListener('submit',(e) => {
 })(); */
 
 
-let valiCorreo = validarCorreo(producto.value);
+/*let valiCorreo = validarCorreo(producto.value);*/
+
+
+
+//VALIDACION DE MODIFICAR PRODUCTO //
+const formulario2 = document.getElementById('Modificarformulario');
+const inputs2 = document.querySelectorAll('#Modificarformulario input');
+
+const expresiones2 = {
+  //Modificarnombre: /^[a-zA-ZÁ-ÿ\s\-9\_\-]{3,25}$/, // Letras, numeros, guion y guion_bajo
+	ModificarNombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.//
+  ModificarDescripcion: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.//
+	ModificarPrecio: /^.{1,100}$/, // 0 a 100 digitos.
+	//correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	//telefono: /^\d{7,14}$/ // 7 a 14 numeros.
+  //ModificarDescripcion: /^[a-zA-Z0-9\_\-]{3,50}$/, // Letras, numeros, guion y guion_bajo
+}
+
+//
+const campos2 = {
+	ModificarNombre: false,
+	ModificarDescripcion: false,
+	ModificarPrecio: false
+}
+
+
+
+// Mostrar y quitar mensaje de que no deben poner caracteres
+const validarCampo2 = (expresion2, input, campo) => {
+	if(expresion2.test(input.value)){
+    /*document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');*/
+    document.querySelector(`#grupo__${campo} .ModificarNombre__input-error`).classList.remove('formulario__input-error-activo');campos2[campo] = true;
+	} else {
+    document.querySelector(`#grupo__${campo} .ModificarNombre__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+}
+}
+
+
+
+
+
+
+//VALIDACIONES POR NAME
+const validarFormulario2 = (m) => {
+	switch (m.target.name) {
+		case "ModificarNombre":
+			validarCampo2(expresiones2.ModificarNombre, m.target, 'ModificarNombre');
+      break;
+
+    case "ModificarPrecio":
+      validarCampo2(expresiones2.ModificarPrecio, m.target, 'ModificarPrecio');
+      break;
+
+    case "ModificarDescripcion":
+      validarCampo2(expresiones2.ModificarDescripcion, m.target, 'ModificarDescripcion');
+      break;
+    }
+  }
+
+  
+
+
+// EVENTO AL LEVANTAR EL DEDO DE LA TECLA VALIDA EL FORMULARIO  
+inputs2.forEach((input) => {
+	input.addEventListener('keyup',validarFormulario2);
+	input.addEventListener('blur',validarFormulario2);
+});
+//PREVENT DEFAULT DE BUTTON
+formulario2.addEventListener('submit', (m) => {
+	m.preventDefault();
+});
+=======
+
 
--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,9 @@ let barra =
         form: "./form_admin.html",
         logo: "./index.html",
         us: "./aboutUs.html",
-        contact: "./contact_us.html"
+        contact: "./contact_us.html",
+        seccion: "./inicioSesion.html",
+        carrito: ""
     };
 
 
@@ -56,7 +58,7 @@ let barra =
     </form>-->
     </div>
     <div class="d-flex">
-      <a href="./inicioSesion.html">
+      <a href="${barra.seccion}">
         <img src="./src/img/usuario.png" class="mr-2" width="30" height="30" src="./usuario.png" alt="Logo"> 
       </a>   
       <a href="#">
@@ -100,9 +102,9 @@ const contenidoFooter =`
                 <div class="col-md-12 col-lg-11 mb-md-0 mb-4">
                     <h2 class="footer-heading">Siguenos</h2>
                     <ul class="ftco-footer-social p-0">
-                        <li class="ftco-animate"><a href="https://www.instagram.com/tutu_wix/?hl=es" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="ion-logo-instagram"><img src="./src/img/iconosRedes/instagram-logo-24.png" alt="icono instagram"></span></a></li>
-                        <li class="ftco-animate"><a href="https://www.facebook.com/tutuwix" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="ion-logo-facebook"><img src="./src/img/iconosRedes/facebook-circle-logo-24.png" alt="icono facebook"></span></a></li>
-                        <li class="ftco-animate"><a href="contact_us.html" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="ion-logo-twitter"><img src="./src/img/iconosRedes/envelope-regular-24.png" alt="icono correo"></span></a></li>
+                        <li class="ftco-animate "><a href="https://www.instagram.com/tutu_wix/?hl=es" data-toggle="tooltip" data-placement="top" title="Instagram"><span class="ion-logo-instagram "><img src="./src/img/iconosRedes/instagram-logo-24.png" alt="icono instagram" class="sizeIcono"></span></a></li>
+                        <li class="ftco-animate "><a href="https://www.facebook.com/tutuwix" data-toggle="tooltip" data-placement="top" title="Facebook"><span class="ion-logo-facebook "><img src="./src/img/iconosRedes/facebook-circle-logo-24.png" alt="icono facebook" class="sizeIcono"></span></a></li>
+                        <li class="ftco-animate "><a href="contact_us.html" data-toggle="tooltip" data-placement="top" title="Twitter"><span class="ion-logo-twitter "><img src="./src/img/iconosRedes/envelope-regular-24.png" alt="icono correo" class="sizeIcono"></span></a></li>
 
                     </ul>
                 </div>
