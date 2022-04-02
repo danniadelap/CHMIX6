bae542d931f8c991a39bde5200e2032587a6c981
EmmanuelRoss <emmanuel.rosales.sanchez@gmail.com>
2022-04-01 23:33:19 -0600

    Se agregan scripts de admin, detallePedido, pedidos

---
+++ src/database/detallePedidoTutu.sql
@@ -0,0 +1,7 @@
+INSERT INTO `databaseTutu`.`detalle_pedido` (`precio`, `cantidad`, `productos_idproductos`, `pedido_idpedido`) VALUES (699,1,1,1);
+INSERT INTO `databaseTutu`.`detalle_pedido` (`precio`, `cantidad`, `productos_idproductos`, `pedido_idpedido`) VALUES (499,1,2,2);
+INSERT INTO `databaseTutu`.`detalle_pedido` (`precio`, `cantidad`, `productos_idproductos`, `pedido_idpedido`) VALUES (579,1,3,3);
+INSERT INTO `databaseTutu`.`detalle_pedido` (`precio`, `cantidad`, `productos_idproductos`, `pedido_idpedido`) VALUES (699,1,4,4);
+INSERT INTO `databaseTutu`.`detalle_pedido` (`precio`, `cantidad`, `productos_idproductos`, `pedido_idpedido`) VALUES (699,1,5,5);
+
+select * from detalle_pedido;
